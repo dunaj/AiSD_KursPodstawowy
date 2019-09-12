054f761c563ef120d977597b33212d3013232dea
Adam Dunajewski <ad.dunajewski@gmail.com>
2019-09-12 14:49:01 +0200

    Implement mergesort class

---
+++ src/pl/adulek/aisd/sorting/MergeSortTest.java
@@ -0,0 +1,19 @@
+package pl.adulek.aisd.sorting;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
+
+public class MergeSortTest {
+    @Test
+    public void sortTest() {
+        int[] arr = {3, 5, -22, 67, -35, 11, 1};
+        int[] expectedArr = {-35, -22, 1, 3, 5, 11, 67};
+
+        MergeSort.sort(arr, 0, arr.length);
+        System.out.println("Sorted array: ");
+        ArrayHelper.printArray(arr);
+        assertArrayEquals(expectedArr, arr);
+    }
+
+}
